bd21c58dca96267a4ca53f753fbbf8030bb56a51
Deblob12 <jeffrey_chiu@college.harvard.edu>
2020-10-02 23:14:48 -0400

    Initial server setup

---
+++ server/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Yes he is!")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
