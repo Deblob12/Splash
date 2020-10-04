c8eea67a837c3aefb3c3c2db7356731aaa883131
Deblob12 <jeffrey_chiu@college.harvard.edu>
2020-10-04 13:19:17 -0400

    Finalized GoogleMaps integration

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 import json
-
+import maps
+from addressDB import storeMapping, getMapping
 from config import firebase
+import re
+import datetime
+from werkzeug.exceptions import abort
+from flask.json import jsonify
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+    return is_token_valid
+
 
 
 app = Flask(__name__)
@@ -47,6 +62,40 @@ def sms_reply():
 
     return str(resp)
 
+
+@app.route('/travel-time', methods=['POST'])
+def travel_time():
+    print(request)
+    if not is_request_valid(request):
+        abort(400)
+    msg = request.form['text']
+    destinations = msg.split(';')
+    for i in range(len(destinations)):
+        destinations[i] = destinations[i].strip().lower()
+    if len(destinations) != 2:
+        return jsonify(
+            repsonse_type='in_channel',
+            text='Please enter starting address and end address.'
+        )
+    address1 = getMapping(request.form['user_id'], destinations[0])
+    address2 = getMapping(request.form['user_id'], destinations[1])
+    if type(address1) is str:
+        destinations[0] = address1
+    if type(address2) is str:
+        destinations[1] = address2
+    
+    try:
+        t = maps.get_directions_duration(destinations[0].strip(), destinations[1].strip())
+    except:
+        return jsonify(
+            response_type='in_channel',
+            text='Invalid Address, Please re-enter.'
+        )
+
+    return jsonify(
+        response_type='in_channel',
+        text= t
+    )
 
 @app.route('/random', methods=['GET', 'POST'])
 def random():
